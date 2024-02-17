d74dde086d1329b6dac466d260048d2e02e05e1d
Sen-Se1 <mbarkihoussem99@gmail.com>
2024-02-17 12:28:46 +0100

    f

--- testGestionBanque.java
+++ testGestionBanque.java
@@ -1,8 +1,8 @@
 public class testGestionBanque {
     public static void main(String[] args) {
-        compte c1 = new compte ("Ammar",1500);
-        compte c2 = new compte ("Ali", 2000);
-        compte c3 = new compteEpargne("Salah", 1000,0.05);
+        compte c1 = new compte("Ammar", 1500);
+        compte c2 = new compte("Ali", 2000);
+        compte c3 = new compteEpargne("Salah", 1000, 0.05);
         System.out.println(c1);
         System.out.println(c2);
         System.out.println(c3);
